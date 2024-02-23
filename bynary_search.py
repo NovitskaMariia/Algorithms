49482d5c23ad25b34587893f8c47013d45e8d92a
Mariia Novytska <mnovickaa@gmail.com>
2024-02-23 19:06:22 +0200

    added binary_search_recursive function

--- bynary_search.py
+++ bynary_search.py
@@ -1,12 +1,12 @@
 # bynary search algoritmth
 
-def binary_search (list, item):
+def binary_search (lst, item):
     low = 0
-    high = len(list) - 1
+    high = len(lst) - 1
 
     while low <= high:
         mid = int((low + high) / 2)
-        guess = list[mid]
+        guess = lst[mid]
         if guess == item:
             return mid
         if guess > item:
@@ -18,3 +18,21 @@ def binary_search (list, item):
 my_list = [x*2 for x in range(100)]
 print(binary_search(my_list, 6))
 print(binary_search(my_list, 17))
+
+
+def binary_search_recursive(lst, low, high, item):
+    if high >= low:
+        mid = (high + low) // 2
+        if lst[mid] == item:
+            return mid
+        elif lst[mid] > item:
+            return binary_search_recursive(lst, low, mid - 1, item)
+        else:
+            return binary_search_recursive(lst, mid + 1, high, item)
+    else:
+        return f"{item} isn't in the list"   
+
+
+# my_list = [x*2 for x in range(100)]
+print(binary_search_recursive(my_list, 0, len(my_list)-1, 6))
+print(binary_search_recursive(my_list, 0, len(my_list)-1, 17))
